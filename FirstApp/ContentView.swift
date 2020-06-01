f1f7de211f477c41576930612db38a6849066f33
Parth Patel <liquidcharcoal@icloud.com>
2020-05-31 22:17:18 -0400

    Initial Commit

---
+++ FirstApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstApp
+//
+//  Created by PARTH PATEL on 5/31/20.
+//  Copyright © 2020 PARTH PATEL. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
