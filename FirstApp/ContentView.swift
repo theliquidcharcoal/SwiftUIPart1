8325c075d3b7a5dbe59b7c54c4b397c446c696ce
theliquidcharcoal <liquidcharcoal@icloud.com>
2020-06-05 23:01:29 -0400

    Initial commit

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -10,7 +10,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Get Done with this project")
+                            .font(.title)
+                            .foregroundColor(.white)
+                            .fontWeight(.semibold)
+                        Text("Get Things Done Easily")
+                            .foregroundColor(Color("accent"))
+                    }
+                    Spacer()
+                    Image("Logo1")
+                }
+                .padding(10)
+    //            .padding(.horizontal, 10)
+    //            .padding(.vertical, 10)
+                Spacer()
+                Image("Card1")
+                .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 300, height: 110, alignment: .top)
+            }
+            .frame(width: 340.0, height: 220.0)
+            .background(Color.black)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+            .padding(20)
+        }
     }
 }
 
@@ -19,3 +47,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+    
